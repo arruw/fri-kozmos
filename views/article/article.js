d31c0b6027c7b3eea1cc84b92b9f8e4d5ccff754
Ziga Cernigoj <zc6857@student.uni-lj.si>
2016-05-04 01:48:16 +0200

    slike pri article.ih

--- views/article/article.js
+++ views/article/article.js
@@ -4,7 +4,37 @@ home.controller('articleCtrl', ['$scope', '$routeParams', '$location', 'getArtic
 function($scope, $routeParams, $location, getArticle) {
     getArticle($routeParams.articleId).then(function(response) {
         $scope.article = response.data;
+
+        var len = $scope.article.images.length;
+        $scope.showID = 1;
+        var vid = false;
+
+        if($scope.article.video != null){
+            $scope.showID = 0;
+            vid = true;
+        }
+
+        $scope.next = function () {
+            console.log("next");
+            $scope.showID += 1;
+            if($scope.showID > len && vid===true){
+                console.log("tuki");
+                $scope.showID = 0;
+            }
+            else if($scope.showID > len && vid===false){
+                $scope.showID = 1;
+            }
+            else if($scope.showID == 0){
+                $scope.showID = 1;
+            }
+            console.log($scope.showID);
+        };
+
     }, function(response) {
         $location.path('/');
     });
+
+
+
+
 }]);
